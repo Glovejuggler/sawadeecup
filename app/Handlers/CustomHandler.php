ef598fce1227e4b2856df209999e200d64188311
Glovejuggler <36526137+Glovejuggler@users.noreply.github.com>
2024-11-07 19:53:58 +0800

    test version 0.0.1

---
+++ app/Handlers/CustomHandler.php
@@ -0,0 +1,19 @@
+namespace App\Handlers;
+
+use Casperlaitw\LaravelFbMessenger\Contracts\BaseHandler;
+use Casperlaitw\LaravelFbMessenger\Messages\ReceiveMessage;
+use Casperlaitw\LaravelFbMessenger\Messages\ButtonTemplate;
+
+class ButtonTemplateHandler extends BaseHandler
+{
+    public function handle(ReceiveMessage $message)
+    {
+        $button = new ButtonTemplate($message->getSender(), 'Default text');
+        $button
+            ->setText('Choose')
+            ->addPostBackButton('First Bbutton')
+            ->addPostBackButton('Second Button')
+            ->addPostBackButton('Third button');
+        $this->send($button);
+    }
+}
